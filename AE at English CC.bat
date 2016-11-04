3317691ac50ff34fd5deebb81471d8faf9a7b62e
Misaki_Akatsuki <mikuchixip@live.jp>
2016-11-04 23:53:40 +0900

    Support CC

---
+++ AE at English CC.bat
@@ -0,0 +1,15 @@
+@echo off
+cd %HOMEPATH%
+cd Documents
+
+type nul >ae_force_english.txt
+
+echo Adobe After Effects CCを英語版で起動します.
+echo 注意:プロンプトが自動終了する前にこのウィンドウを閉じると
+echo      ae_force_english.txtが削除されません.
+
+start "" "C:\Program Files\Adobe\Adobe After Effects CC\Support Files\AfterFx.exe"
+
+ping localhost -n 5 > nul
+
+del ae_force_english.txt
--- README.txt
+++ README.txt
@@ -11,4 +11,6 @@
   Windows版 Adobe After Effects CS4以上
 
 バージョン情報
+  2013/6/18 Ver 1.1.0
+    After Effects CCに対応
   2012/7/27 Ver 1.0.0
